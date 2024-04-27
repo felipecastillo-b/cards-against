a941360b52f3ec74d426bda7a3928d87e6cc7aca
Felipe Castillo <34404062+felipecastillo-b@users.noreply.github.com>
2024-04-27 16:12:47 -0400

    Update Header.jsx

--- www/src/components/Header.jsx
+++ www/src/components/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
     <header className="flex p-3">
       <Link to="/" className="flex items-center space-x-2">
         <img src={logo} width={36} height={36} alt="logo" />
-        <h1 className="text-2xl">CCW</h1>
+        <h1 className="text-2xl">CCC</h1>
       </Link>
       <div className="flex-grow"></div>
       {/* <Select options={OPTIONS} selected={selected} onChange={setSelected} className="w-20 text-gray-700" /> */}
